68ffc4e939346516fbee634aa7861f3fcf893060
diogoc <diogoc@motorola.com>
2016-08-10 20:08:00 -0300

    bluetooth helper base

---
+++ app/src/main/java/br/org/cesar/armrobotester/helper/Constants.java
@@ -0,0 +1,19 @@
+package br.org.cesar.armrobotester.helper;
+
+/**
+ * Created by dlmc on 09/08/16.
+ */
+
+public interface Constants {
+
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+}
