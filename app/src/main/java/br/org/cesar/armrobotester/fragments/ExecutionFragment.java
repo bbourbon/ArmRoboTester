f8d1b052d0aee05381183a35f37275f91e3e9e41
Bruno Bourbon <bcb@ANSELAZIO.local>
2016-11-20 13:11:06 -0300

    Execution Fragment

--- app/src/main/java/br/org/cesar/armrobotester/fragments/ExecutionFragment.java
+++ app/src/main/java/br/org/cesar/armrobotester/fragments/ExecutionFragment.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,14 +55,37 @@ public class ExecutionFragment extends Fragment {
         ArrayList<ExecutionSuite> testSuites = new ArrayList<>(8);
 
         // --- Test Suite ID_001 ---
-
+        ExecutionSuite es001 = new ExecutionSuite("ID_001");
+        es001.setDone();
+        testSuites.add(es001);
         // --- Test Suite ID_002 ---
+        ExecutionSuite es002 = new ExecutionSuite("ID_002");
+        es002.setDone();
+        testSuites.add(es002);
         // --- Test Suite ID_003 ---
+        ExecutionSuite es003 = new ExecutionSuite("ID_003");
+        es003.setProgress();
+        testSuites.add(es003);
         // --- Test Suite ID_004 ---
+        ExecutionSuite es004 = new ExecutionSuite("ID_004");
+        es004.setWaiting();
+        testSuites.add(es004);
         // --- Test Suite ID_005 ---
+        ExecutionSuite es005 = new ExecutionSuite("ID_005");
+        es005.setWaiting();
+        testSuites.add(es005);
         // --- Test Suite ID_006 ---
+        ExecutionSuite es006 = new ExecutionSuite("ID_006");
+        es006.setWaiting();
+        testSuites.add(es006);
         // --- Test Suite ID_007 ---
+        ExecutionSuite es007 = new ExecutionSuite("ID_007");
+        es007.setWaiting();
+        testSuites.add(es007);
         // --- Test Suite ID_008 ---
+        ExecutionSuite es008 = new ExecutionSuite("ID_008");
+        es008.setWaiting();
+        testSuites.add(es008);
 
         return testSuites;
 
@@ -73,6 +98,8 @@ public class ExecutionFragment extends Fragment {
         List<ExecutionTest> testCases;
         String status;
         boolean hasExecuted;
+        boolean isInProgess;
+        boolean isWaiting;
 
         public ExecutionSuite (String name) {
             this.name = name;
@@ -85,16 +112,22 @@ public class ExecutionFragment extends Fragment {
         void setWaiting() {
             this.status = "(waiting)";
             this.hasExecuted = false;
+            this.isInProgess = false;
+            this.isWaiting = true;
         }
 
         void setProgress() {
             this.status = "(in progress)";
             this.hasExecuted = false;
+            this.isInProgess = true;
+            this.isWaiting = false;
         }
 
         void setDone() {
             this.status = "";
             this.hasExecuted = true;
+            this.isInProgess = false;
+            this.isWaiting = false;
         }
 
     }
@@ -147,27 +180,27 @@ public class ExecutionFragment extends Fragment {
 
         @Override
         public int getChildrenCount(int groupPosition) {
-            return 0;
+            return mExecutionSuitesList.get(groupPosition).testCases.size();
         }
 
         @Override
         public Object getGroup(int groupPosition) {
-            return null;
+            return mExecutionSuitesList.get(groupPosition);
         }
 
         @Override
         public Object getChild(int groupPosition, int childPosition) {
-            return null;
+            return mExecutionSuitesList.get(groupPosition).testCases.get(childPosition);
         }
 
         @Override
         public long getGroupId(int groupPosition) {
-            return 0;
+            return groupPosition;
         }
 
         @Override
         public long getChildId(int groupPosition, int childPosition) {
-            return 0;
+            return childPosition;
         }
 
         @Override
@@ -178,7 +211,43 @@ public class ExecutionFragment extends Fragment {
         @Override
         public View getGroupView(int groupPosition, boolean isExpanded, View convertView,
                                  ViewGroup parent) {
-            return null;
+
+            ExecutionSuite suite = mExecutionSuitesList.get(groupPosition);
+
+            if (convertView == null) {
+                LayoutInflater layoutInflater = (LayoutInflater) this.mContext
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = layoutInflater.inflate(R.layout.list_execution_item, null);
+            }
+
+            TextView txtListChild = (TextView) convertView
+                    .findViewById(R.id.text_item_id);
+
+            txtListChild.setText("Suite " +suite.name);
+
+            ImageView blueDot = (ImageView) convertView.findViewById(R.id.image_group_dot);
+            ImageView check = (ImageView) convertView.findViewById(R.id.image_group_check_right);
+            TextView textStatus = (TextView) convertView.findViewById(R.id.text_status);
+
+            textStatus.setText(suite.status);
+
+            if (suite.hasExecuted) {
+                blueDot.setVisibility(View.VISIBLE);
+                check.setVisibility(View.VISIBLE);
+                textStatus.setVisibility(View.GONE);
+            } else if (suite.isInProgess) {
+                blueDot.setVisibility(View.VISIBLE);
+                check.setVisibility(View.GONE);
+                textStatus.setVisibility(View.VISIBLE);
+            } else if (suite.isWaiting) {
+                blueDot.setVisibility(View.GONE);
+                check.setVisibility(View.GONE);
+                textStatus.setVisibility(View.VISIBLE);
+            }
+
+
+            return convertView;
+
         }
 
         @Override
